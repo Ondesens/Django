8a749b8a910a2e89e4deae713e93c79cc5b0191f
Ondesens <vovan.ilin.pskov.128@gmail.com>
2026-04-20 17:32:39 +0300

    Update urls.py

--- django_work/blog/urls.py
+++ django_work/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path("", views.home, name="home"),  # 👈 теперь главная
-    path("blog/", views.index, name="index"),  # список новостей
+    path("", views.home, name="home"), 
+    path("blog/", views.index, name="index"),
     path("detail/<int:pk>/", views.detail, name="blog_detail"),
     path("create/", views.create_blog, name="blog_create"),
     path("update/<int:pk>/", views.update_blog, name="update_blog"),
